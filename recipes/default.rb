8103c1ff89a91e7e16e60898911ac1d20128d373
Steven Wade <steven@stevenwade.co.uk>
2016-03-02 17:03:49 +0000

    Adding necessary resources to the default recipe.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,3 +7,28 @@
 node.override['gocd']['agent']['go_server_host'] = 'gocd.ukpds.org'
 
 include_recipe 'gocd::agent'
+include_recipe 'chocolatey'
+
+# Construct the ".chef" directory.
+powershell_script 'write-to-interpolated-path' do
+  code <<-EOH
+  New-Item -ItemType Directory -Force -Path C:/.chef
+  EOH
+end
+
+# Drop the knife.rb and client.pem file in the ".chef" directory.
+cookbook_file 'C:/.chef/knife.rb' do
+  source 'knife.rb'
+  action :create_if_missing
+end
+
+cookbook_file 'C:/chef/parliamentary-digital-service-validator.pem' do
+  source 'parliamentary-digital-service-validator.pem'
+  action :create_if_missing
+end
+
+# Install chef related software
+chocolatey 'chefdk'
+
+# Install git as we need this to download repositories.
+chocolatey 'git'
